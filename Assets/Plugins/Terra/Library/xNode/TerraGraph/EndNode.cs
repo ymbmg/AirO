ab08448a05d5a98960c03ea48cd880d40da43b57
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2024-03-24 17:14:33 -0400

    imported Terra and fixed minor issues

---
+++ Assets/Plugins/Terra/Library/xNode/TerraGraph/EndNode.cs
@@ -0,0 +1,18 @@
+﻿using Terra.CoherentNoise;
+using Terra.Graph.Noise;
+using UnityEngine;
+
+namespace Terra.Graph {
+	[CreateNodeMenu("End Generator")]
+	public class EndNode: XNode.Node {
+		[Input(ShowBackingValue.Never, ConnectionType.Override)] public AbsGeneratorNode Noise;
+
+		/// <summary>
+		/// Returns the "final" generator attached to this 
+		/// node's input
+		/// </summary>
+		public Generator GetFinalGenerator() {
+			return GetInputValue<AbsGeneratorNode>("Noise").GetGenerator();
+		}
+	}
+}
---
+++ Assets/Plugins/Terra/Library/xNode/TerraGraph/Noise/Editor/GeneratorNodeEditor.cs
@@ -0,0 +1,20 @@
+﻿using Terra.Graph.Noise;
+using UnityEditor;
+using XNodeEditor;
+
+namespace Terra.Editor.Graph {
+	[CustomNodeEditor(typeof(AbsGeneratorNode))]
+	class GeneratorNodeEditor: NodeEditor {
+		public override void OnBodyGUI() {
+			EditorGUI.BeginChangeCheck();
+			base.OnBodyGUI();
+			if (EditorGUI.EndChangeCheck()) {
+				((AbsGeneratorNode)target).OnValueChange();
+			}
+		}
+
+		public override string GetTitle() {
+			return ((AbsGeneratorNode)target).GetTitle();
+		}
+	}
+}
